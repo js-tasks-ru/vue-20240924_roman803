a08d3b6eac1da3b9ce6ec5ce6fb8257d391317fe
rmirskov <rmir050607@gmail.com>
2024-09-28 10:51:59 +0300

    Added solution for task #create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,22 @@
 import { defineComponent, createApp } from 'vue'
+
+const DateApp = defineComponent({
+  name: 'DateApp',
+
+  setup() {
+    const dateOptions = { dateStyle: 'long' }
+
+    function getLocalCurrentDate(options = dateOptions) {
+      return new Date().toLocaleDateString(navigator.language, options)
+    }
+
+    return {
+      getLocalCurrentDate,
+    }
+  },
+
+  template: '<div>Сегодня {{ getLocalCurrentDate() }}</div>',
+})
+
+const dateApp = createApp(DateApp)
+const vm = dateApp.mount('#app')
