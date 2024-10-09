ccda62c79d9822a00f3dd2999295c3bebf27d66a
rmirskov <rmir050607@gmail.com>
2024-10-09 15:06:28 +0300

    Added solutions for tasks #MeetupView, #UiClock, #Удаляемые Email-ы, #Компонент-счётчик, #компоненты: погода в Средиземье

--- 03-components/30-removable-emails/EmailList.js
+++ 03-components/30-removable-emails/EmailList.js
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue'
+import { ref, defineComponent, computed } from 'vue'
 import EmailListItem from './EmailListItem.js'
 
 export default defineComponent({
@@ -15,6 +15,8 @@ export default defineComponent({
     },
   },
 
+  emits: ['removeItem'],
+
   template: `
     <ul class="emails-list unstyled-list" aria-label="Emails">
       <EmailListItem
@@ -22,6 +24,7 @@ export default defineComponent({
         :key="email"
         :email="email"
         :marked="isMarked"
+        @get-index="() => $emit('removeItem', index)"
       />
     </ul>
   `,
